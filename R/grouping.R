cfd82a80253aad0b82ceb21bbe0687115fc5ca21
Kevin Stout <krstout@buffalo.edu>
2020-06-26 12:27:53 -0400

    matrix differences and matrix addition

--- R/grouping.R
+++ R/grouping.R
@@ -1,3 +1,4 @@
+
 az <- county_table("county", 2018, "AZ")
 
 az$race_all_n <- scale(az$race_all, scale = T)
@@ -9,3 +10,15 @@ az$race_all_n <- (az$race_all-min(az$race_all))/(max(az$race_all)-min(az$race_al
 summary(az$race_all_n)
 
 # probably better to normalize variables rather than scale...not completely sure
+
+d1 <- abs(outer(az$p.white, az$p.white, '-'))
+d2 <- abs(outer(az$p.black, az$p.black, '-'))
+d3 <- abs(outer(az$p.aian, az$p.aian, '-'))
+d4 <- abs(outer(az$p.asian, az$p.asian, '-'))
+d5 <- abs(outer(az$p.nhpi, az$p.nhpi, '-'))
+d6 <- abs(outer(az$p.other, az$p.other, '-'))
+d7 <- abs(outer(az$p.race2, az$p.race2, '-'))
+
+matrix.list <- list(d1, d2, d3, d4, d5, d6, d7)
+
+d8 <- Reduce('+', matrix.list)
